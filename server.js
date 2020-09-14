f5565f509a17e68864749923d151390e6c7f2f87
Andrew Hardemon <aeroatlas@ymail.com>
2020-09-13 19:38:26 -0700

    added express to the server.js

--- server.js
+++ server.js
@@ -1,50 +1,51 @@
-const http = require('http');
-const fs = require('fs'); //filesystem
+// const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req,res) => {
-  // console.group(req.url, req.method, req.headers)
-  // process.exit()
-  const url = req.url
-  const method = req.method
-  if(url === '/'){
-    res.write('<html>')
-    res.write('<head><title>Enter Message</title></head>')
-    res.write(`<body>
-      <form action="/message" method="POST">
-        <input type="text" name="message">
-          <button type="submit">SEND</button>
-        </input>
-      </form></body>`)
-    res.write('</html>')
-    return res.end();
-  }
+const app = express();
+// const server = http.createServer(app);
 
-  if(url === '/message' && method === 'POST'){
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log(chunk)
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1]
-      console.log(parsedBody)
-      fs.writeFileSync('message.txt', message);
-    })
-    res.statusCode = 302;
-    res.setHeader('Location', '/'); //302 is redirect
-    return res.end();
-  }
-
-  res.setHeader('Content-Type', 'text/html')
-  res.write('<html>')
-  res.write('<head><title>Enter Message</title></head>')
-  res.write(`<body><h1>Hello</h1></body>`)
-  res.write('</html>')
-  res.end();
+app.use('/', (req,res,next)=>{
+  console.log('This always runs')
+  next()
 })
 
-server.listen(3000)
+app.use('/add-product', (req, res, next) => {
+  console.log("In the 2nd middleware");
+  res.send('<form><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log("In the 2nd middleware");
+  res.send('<h1>Hello from express</h1>');
+});
+
+app.listen(3000);
+
+//app.use([path,]callback[,callback])...
+
+
+
+
+
+//Client -> Request -> Server -> Response -> Client
 
 
 //Stream -> ReqBody1 => ReqBody2 => Buffer[ReqBody3 => ReqBody4] -> Fully Parsed
+
+/*Incoming Req -> MyCode + Single JS Thread 
+                            ->Event Loop(only finish fast finishing code) -> Handle Event Callbacks 
+                            ->"fs" -sent to-> WorkerPool (Does the heavy lifting & runs of diff threads)
+                                                  ->^ triggers callback to EventLoop
+Event Loop  -> Timers(Execute setTimeout, setInterval Callback)
+            -> Pending Callbacks (Execute I/O-related (input/output-disk + network operations ~blocking ops). Callbacks that were defered)
+              ( if too many callbacks it will skip a few and give those callback to the next loop )
+            -> Poll (retrieve new I/O events, execute their callbacks). If not possible defer to pending cb's and check for timers cb's
+            -> Check (execute setImmediate() callbacks) executes immediate after any open callbacks. faster than settimeout. after current cycle
+            -> Close Callbaccks (execute all close event callbacks)
+            --> process.exit (if (refs == 0)) [refs is for every new callback] -1 ever completed callback
+                ** Listen doesn't let refs decrease so it stays looping
+*/
+
+/*
+Middleware -> Request -> Middleware () -> Response
+*/
